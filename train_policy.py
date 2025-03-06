ab3ef549c93109620355d7ad65210f9ed820e76d
Alison Bartsch <abartsch@andrew.cmu.edu>
2025-03-06 15:56:29 -0500

    updated script

--- train_policy.py
+++ train_policy.py
@@ -14,7 +14,7 @@ import torch
 
 
 # exp name
-exp_name = 'pottery_12pred_with_augs'
+exp_name = 'pottery_4pred_with_augs'
 ckpt_dir = 'checkpoints/' + exp_name
 # if ckpt_dir does not exist, create it
 if not os.path.exists(ckpt_dir):
@@ -37,7 +37,7 @@ projection_head = EncoderHead(encoded_dim, latent_dim).to(device)
 # define the dataloader
 n_datapoints = 1800 # the desired numer of datapoints after augmentation
 n_raw_trajectories = 5 # the number of raw datapoints
-pred_horizon = 12
+pred_horizon = 4
 num_epochs = 750
 target_shape = "pottery" # ["Line", "X", "Cone", or "All_Shapes"] # TODO: select what shape target you are training for
 dataset_path = '/home/alison/Documents/Feb26_Human_Demos_Raw/pottery/'
