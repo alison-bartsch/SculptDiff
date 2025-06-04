bd445390674270aa524f2953a76dbc8d31417481
Alison Bartsch <abartsch@andrew.cmu.edu>
2025-06-04 16:55:35 -0400

    created more gifs

--- test_collision_checker.py
+++ test_collision_checker.py
@@ -1,5 +1,34 @@
 import numpy as np
 import open3d as o3d
+from tqdm import tqdm
+
+def create_grippers(action7d):
+    # Create two cylinders for the gripper fingers
+    cylinder_radius = 0.006 #4 # 4  # 8mm diameter
+    cylinder_length = 0.045  # 45mm length
+    cylinder1 = o3d.geometry.TriangleMesh.create_cylinder(radius=cylinder_radius, height=cylinder_length)
+    cylinder1.compute_vertex_normals()
+    cylinder1.paint_uniform_color([0.8, 0.8, 0.8])  # Set color to light gray
+    cylinder1.translate([-cylinder_length / 2, 0, 0])  # Center the cylinder at the origin
+    cylinder1.rotate(o3d.geometry.get_rotation_matrix_from_xyz((0, 0, np.pi / 2)))  # Rotate to align with the y-axis
+    cylinder2 = o3d.geometry.TriangleMesh.create_cylinder(radius=cylinder_radius, height=cylinder_length)
+    cylinder2.compute_vertex_normals()
+    cylinder2.paint_uniform_color([0.8, 0.8, 0.8])  # Set color to light gray
+    cylinder2.translate([-cylinder_length / 2, 0, 0])  # Center the cylinder at the origin
+    cylinder2.rotate(o3d.geometry.get_rotation_matrix_from_xyz((0, 0, np.pi / 2)))  # Rotate to align with the y-axis
+    # cylinder2.translate([0.04, 0, 0])  # Move the second cylinder to the right
+    cylinder2.translate([0.05, 0, 0])  # Move the second cylinder to the right
+
+    # Apply the action7d transformations
+    translation = action7d[:3]  # x, y, z translation
+    rotation = action7d[3:6]  # roll, pitch, yaw in degrees
+    rotation_matrix = o3d.geometry.get_rotation_matrix_from_xyz(np.radians(rotation))
+
+    cylinder1.rotate(rotation_matrix, center=(0, 0, 0))  # Apply rotation around the origin
+    cylinder1.translate(translation)  # Apply translation
+    cylinder2.rotate(rotation_matrix, center=(0, 0, 0))  # Apply rotation around the origin
+    cylinder2.translate(translation)  # Apply translation
+    return cylinder1, cylinder2
 
 def check_finger_collision(action7d, pcl, vis=False):
     '''
@@ -59,7 +88,8 @@ def check_finger_collision(action7d, pcl, vis=False):
     collision_count = c1_collision_count + c2_collision_count
 
     # return True if either finger collides with the point cloud
-    if collision_count > 50:  # threshold for collision detection
+    if collision_count > 5:  # threshold for collision detection
+        print("Collision count: ", collision_count)
         return True
     else:
         return False
@@ -78,7 +108,7 @@ def check_bbox_collision(pcl, cylinder):
     collision_count = np.sum(np.all((points >= bounding_box.min_bound) & (points <= bounding_box.max_bound), axis=1))
     return collision_count
 
-def check_collision(pcl, cylinder, epsilon=0.001):
+def check_collision_new_o3d_version(pcl, cylinder, epsilon=0.001):
     '''
     Check for collision between the cylinder and the point cloud.
     A collision is defined if any point in the point cloud lies within
@@ -90,22 +120,7 @@ def check_collision(pcl, cylinder, epsilon=0.001):
     triangles = np.asarray(cylinder.triangles)
     vertices = np.asarray(cylinder.vertices)
     collision_count = 0
-    # for point in points:
-    #     for triangle in triangles:
-    #         v0 = vertices[triangle[0]]
-    #         v1 = vertices[triangle[1]]
-    #         v2 = vertices[triangle[2]]
-    #         # Calculate the area of the triangle
-    #         area = np.linalg.norm(np.cross(v1 - v0, v2 - v0)) / 2.0
-    #         # Calculate the distance from the point to the triangle
-    #         d = np.abs(np.dot(np.cross(v1 - v0, v2 - v0), point - v0)) / (area + epsilon)
-    #         if d < epsilon:
-    #             collision_count += 1
-
-    # point-mesh containment test
-    # for all points choose a random ray extending to infinity and calculate the number of intersections of that ray with the mesh
-    # if the number of intersections is odd, the point is inside the mesh
-    # if the number of intersections is even, the point is outside the mesh
+    
     for point in points:
         random_direction = np.random.rand(3)  # Random direction vector
         p1 = point
@@ -125,35 +140,116 @@ def check_collision(pcl, cylinder, epsilon=0.001):
         if len(ray_mesh_intersections) % 2 == 1:  # odd number of intersections means point is inside the mesh
             collision_count += 1
     return collision_count
+
+def intersect_segment_triangle(p0, p1, v0, v1, v2):
+    EPSILON = 1e-8
+    dir = p1 - p0
+    edge1 = v1 - v0
+    edge2 = v2 - v0
+    h = np.cross(dir, edge2)
+    a = np.dot(edge1, h)
+    if -EPSILON < a < EPSILON:
+        return False  # Ray is parallel to triangle
+    f = 1.0 / a
+    s = p0 - v0
+    u = f * np.dot(s, h)
+    if u < 0.0 or u > 1.0:
+        return False
+    q = np.cross(s, edge1)
+    v = f * np.dot(dir, q)
+    if v < 0.0 or u + v > 1.0:
+        return False
+    t = f * np.dot(edge2, q)
+    if t < 0.0 or t > 1.0:
+        return False  # Outside the segment
+    return True  # Intersection found
+
+
+def check_collision(pcl, cylinder, epsilon=0.001):
+    '''
+    Check for collision between the cylinder and the point cloud.
+    A collision is defined if any point in the point cloud lies within
+    the gripper mesh. Returns True if there is a collision, False otherwise.
+    '''
+    # point-mesh epsilon intersection test
+    # for all points perform a point-triangle distance check against all triangles in the cylinder mesh
+    points = np.asarray(pcl.points)
+    triangles = np.asarray(cylinder.triangles)
+    vertices = np.asarray(cylinder.vertices)
+    collision_count = 0
+    
+    for point in tqdm(points):
+        random_direction = np.random.rand(3)  # Random direction vector
+        p1 = point
+        p2 = point + random_direction*1e6
+
+        # get intersections
+        ray_mesh_intersections = 0
+        for tri in triangles:
+            v0, v1, v2 = vertices[tri]
+            if intersect_segment_triangle(p1, p2, v0, v1, v2):
+                ray_mesh_intersections += 1
+
+        if ray_mesh_intersections % 2 == 1:  # odd number of intersections means point is inside the mesh
+            collision_count += 1
+    return collision_count
+    
     
 if __name__ == "__main__":
-    # Example action7d input
-    action7d = np.array([0.6, 0.0, 0.1, 40.0, 20.0, 45.0, 0.001])  # Example action7d input
-    np_pcl = np.load('/home/alison/Documents/Mar24_Bowl_Demos_Soft_Finger/pottery/Trajectory4/unnormalized_pointcloud22.npy') # 22.npy')  # Load a sample point cloud
-    pcl = o3d.geometry.PointCloud()
-    pcl.points = o3d.utility.Vector3dVector(np_pcl)
+    # # Example action7d input
+    # action7d = np.array([0.6, 0.0, 0.1, 40.0, 20.0, 45.0, 0.001])  # Example action7d input
+    # np_pcl = np.load('/home/alison/Documents/Mar24_Bowl_Demos_Soft_Finger/pottery/Trajectory4/unnormalized_pointcloud22.npy') # 22.npy')  # Load a sample point cloud
+    # pcl = o3d.geometry.PointCloud()
+    # pcl.points = o3d.utility.Vector3dVector(np_pcl)
 
-    # create and visualize the gripper mesh
-    collision = check_finger_collision(action7d, pcl, vis=True)
-    if collision:
-        print("Collision detected!")
-    else:
-        print("No collision detected.")
+    # # create and visualize the gripper mesh
+    # collision = check_finger_collision(action7d, pcl, vis=True)
+    # if collision:
+    #     print("Collision detected!")
+    # else:
+    #     print("No collision detected.")
+
+    # np_pcl = np.load('/home/alison/Documents/Mar24_Bowl_Demos_Soft_Finger/pottery/Trajectory0/unnormalized_pointcloud2.npy') # 22.npy')  # Load a sample point cloud
+    # pcl = o3d.geometry.PointCloud()
+    # pcl.points = o3d.utility.Vector3dVector(np_pcl)
+
+    # action7d = np.load('/home/alison/Documents/Mar24_Bowl_Demos_Soft_Finger/pottery/Trajectory0/action7d_unnormalized1.npy')  # Load the corresponding action
+    # action7d[2] -= 0.04
+    # action7d[5] += 90
+    # collision = check_finger_collision(action7d, pcl, vis=True)
+
+    # a2 = np.load('/home/alison/Documents/Mar24_Bowl_Demos_Soft_Finger/pottery/Trajectory0/action7d_unnormalized1.npy')  # Load the corresponding action
+    # a2[0] -= 0.05
+    # a2[1] -= 0.025
+    # a2[2] -= 0.04
+    # # a2[5] += 90
+    # collision = check_finger_collision(a2, pcl, vis=True)
+
+    # a3 = np.load('/home/alison/Documents/Mar24_Bowl_Demos_Soft_Finger/pottery/Trajectory0/action7d_unnormalized1.npy')  # Load the corresponding action
+    # a3[0] += 0.03
+    # a3[1] += 0.025
+    # a3[2] -= 0.04
+    # a3[5] += 90
+    # collision = check_finger_collision(a3, pcl, vis=True)
+
 
     for i in range(1,60):
         np_pcl = np.load('/home/alison/Documents/Mar24_Bowl_Demos_Soft_Finger/pottery/Trajectory0/unnormalized_pointcloud' + str(i) + '.npy') # 22.npy')  # Load a sample point cloud
+        # downsample the point cloud to 1000 points
+        np_pcl = np_pcl[np.random.choice(np_pcl.shape[0], 250, replace=False), :]
+        
         pcl = o3d.geometry.PointCloud()
         pcl.points = o3d.utility.Vector3dVector(np_pcl)
 
         action7d = np.load('/home/alison/Documents/Mar24_Bowl_Demos_Soft_Finger/pottery/Trajectory0/action7d_unnormalized' + str(i-1) + '.npy')  # Load the corresponding action
-        # add 0.03 offset to the x coordinate of the action
-        action7d[0] += 0.0225 # 0.025
-        # add 0.01 offset to the y coordinate of the action
-        action7d[1] -= 0.0225 
-        # change the z coordinate of the action to be - 0.045
+    
+
+        # add offsets
+        action7d[0] += 0.03
+        action7d[1] -= 0.025
         action7d[2] -= 0.04
-        # change the rotation about the z-axis to add 90 degrees
         action7d[5] += 90
+
         collision = check_finger_collision(action7d, pcl, vis=True)
         if collision:
             print(f"Collision detected for point cloud {i-1}!")
---
+++ vis_trajectory_with_collision.py
@@ -0,0 +1,196 @@
+import cv2
+import time
+import numpy as np
+import open3d as o3d
+from PIL import Image, ImageChops
+import matplotlib.pyplot as plt
+from test_collision_checker import create_grippers
+
+def animate_point_cloud(traj_idx, traj_len, view='isometric', pltmap='viridis'):
+    '''
+    This function takes a point cloud and generates an animated gif with the point cloud
+    rotating around the z-axis. The camera remains in a fixed observation pose.
+    '''
+    img_sequence = []
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(width=1920, height=1080)
+
+    # set the point size
+    ro = vis.get_render_option()
+    ro.point_size = 10.0
+
+    # set the camera view
+    ctr = vis.get_view_control()
+    base_path = "/home/alison/Documents/GitHub/subgoal_diffusion/open3d_configs"
+    if view == 'isometric':
+        path = base_path + "/isometric_view.json"
+        print("Using isometric view.")
+    elif view == 'side':
+        path = base_path + "/side_on_view.json"
+    elif view == 'top':
+        path = base_path + "/top_down_view.json"
+    else:
+        raise ValueError("Invalid view type. Choose 'isometric', 'side', or 'top'.")
+    print("Loading camera parameters from:", path)
+    parameters = o3d.io.read_pinhole_camera_parameters(path)
+
+    # add in the geometry
+    np_pcl = np.load('/home/alison/Documents/Mar24_Bowl_Demos_Soft_Finger/pottery/Trajectory' + str(traj_idx) + '/unnormalized_pointcloud1.npy') # 22.npy')  # Load a sample point cloud
+    pcl = o3d.geometry.PointCloud()
+    pcl.points = o3d.utility.Vector3dVector(np_pcl)
+
+    action7d = np.load('/home/alison/Documents/Mar24_Bowl_Demos_Soft_Finger/pottery/Trajectory' + str(traj_idx) + '/action7d_unnormalized0.npy')  # Load the corresponding action
+    # add offsets
+    action7d[0] += 0.03
+    action7d[1] -= 0.025
+    action7d[2] -= 0.04
+    action7d[5] += 90
+    cylinder1, cylinder2 = create_grippers(action7d)
+    vis.add_geometry(pcl)
+    vis.add_geometry(cylinder1)
+    vis.add_geometry(cylinder2)
+
+    img = vis.capture_screen_float_buffer()
+    img_sequence.append(img)
+
+    ctr.convert_from_pinhole_camera_parameters(parameters, True)
+    ctr.set_zoom(1.15)
+    time.sleep(0.25)
+
+
+    for i in range(2,traj_len):
+        # vis.remove_geometry(pcl)
+        # vis.remove_geometry(cylinder1)
+        # vis.remove_geometry(cylinder2)
+
+        np_pcl = np.load('/home/alison/Documents/Mar24_Bowl_Demos_Soft_Finger/pottery/Trajectory' + str(traj_idx) + '/unnormalized_pointcloud' + str(i) + '.npy') # 22.npy')  # Load a sample point cloud
+        # pcl = o3d.geometry.PointCloud()
+        pcl.points = o3d.utility.Vector3dVector(np_pcl)
+
+        action7d = np.load('/home/alison/Documents/Mar24_Bowl_Demos_Soft_Finger/pottery/Trajectory' + str(traj_idx) + '/action7d_unnormalized' + str(i-1) + '.npy')  # Load the corresponding action
+        # add offsets
+        action7d[0] += 0.03
+        action7d[1] -= 0.025
+        action7d[2] -= 0.04
+        action7d[5] += 90
+        c1, c2 = create_grippers(action7d)
+
+        # update the gripper geometries
+        cylinder1.vertices = c1.vertices
+        cylinder1.triangles = c1.triangles
+        cylinder2.vertices = c2.vertices
+        cylinder2.triangles = c2.triangles
+
+        # Update the visualizer by removing old geometries and adding new ones
+        # vis.add_geometry(pcl)
+        # vis.add_geometry(cylinder1)
+        # vis.add_geometry(cylinder2)
+        vis.update_geometry(pcl)
+        vis.update_geometry(cylinder1)
+        vis.update_geometry(cylinder2)
+        vis.poll_events()
+        vis.update_renderer()
+
+        # Capture the screen
+        img = vis.capture_screen_float_buffer()
+        img_sequence.append(img)
+
+        time.sleep(0.25)
+    
+    # close the visualizer
+    vis.destroy_window()
+    return img_sequence
+
+def set_camera_to_orthographic(vis):
+    # get current camra settings
+    ctr = vis.get_view_control()
+    param = ctr.convert_to_pinhole_camera_parameters()
+    # modify the intrinsic parameters to achieve orthographic projection
+    param.intrinsic.set_intrinsics(
+        width=1920,
+        height=1080,
+        fx=1.0,
+        fy=1.0,
+        cx=960,
+        cy=540
+    )
+    ctr.convert_from_pinhole_camera_parameters(param)
+
+def generate_colormap(pcl, pltmap='viridis'):
+    '''
+    This function takes a point cloud and generates a colormap based on the z-coordinate of each point.
+    The colormap is then applied to the point cloud and visualized.
+    '''
+    # Normalize the z-coordinates to the range [0, 1]
+    z = pcl[:, 2]
+    z_min = np.min(z)
+    z_max = np.max(z)
+    z_normalized = (z - z_min) / (z_max - z_min)
+
+    # Create a colormap
+    colormap = plt.get_cmap(pltmap)
+    colors = colormap(z_normalized)
+
+    return colors[:, :3]  # Ignore the alpha channel
+
+def vis_fov_point_cloud(pcl):
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(width=1920, height=1080)
+
+    # setr the point size
+    ro = vis.get_render_option()
+    ro.point_size = 10.0
+
+    # set the camera view
+    ctr = vis.get_view_control()
+    parameters = o3d.io.read_pinhole_camera_parameters("/home/alison/Documents/GitHub/subgoal_diffusion/open3d_configs/isometric_view.json")
+
+    # add in the geometry
+    geometry = o3d.geometry.PointCloud()
+    geometry.points = o3d.utility.Vector3dVector(pcl)
+    geometry.colors = o3d.utility.Vector3dVector(generate_colormap(pcl))
+    vis.add_geometry(geometry) #, reset_bounding_box=False)
+
+    ctr.convert_from_pinhole_camera_parameters(parameters, True)
+
+    vis.run()
+    vis.destroy_window()
+
+def make_video(img_sequence, filename='point_cloud_animation.mp4', fps=5):
+    '''
+    This function takes a list of images and creates a video from them.
+    '''
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    height, width, _ = np.array(img_sequence[0]).shape
+    out = cv2.VideoWriter(filename, fourcc, fps, (width, height))
+
+    for img in img_sequence:
+        img = np.array(img)
+        img = (img * 255).astype(np.uint8)
+        out.write(img)
+
+    out.release()
+
+def make_gif(img_sequence, filename='point_cloud_animation.gif', duration=100):
+    '''
+    This function takes a list of images and creates a gif from them.
+    '''
+    images = []
+    for img in img_sequence:
+        img = np.array(img)
+        img = img[::-1, :, :]  # flip the colors from BGR to RGB <--- this is close, but flips the image upside down
+        # flip the image upside down
+        img = np.flipud(img)
+        img = (img * 255).astype(np.uint8)
+        images.append(Image.fromarray(img))
+    images[0].save(filename, save_all=True, append_images=images[1:], optimize=False, duration=duration, loop=0)
+
+if __name__ == "__main__":
+    traj_idx = 5 # 0, 1, 2, 3, 4, 5, 6
+    traj_len = 20 # 64, 22, 33, 32, 22, 20, 33  # Number of frames in the trajectory
+    
+    img_list = animate_point_cloud(traj_idx, traj_len, view='isometric')
+    print("Generated {} images for the animation.".format(len(img_list)))
+    make_gif(img_list, filename='pottery_traj' + str(traj_idx) + '.gif', duration=250)
+    # vis_fov_point_cloud(pcl)
+
