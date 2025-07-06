7b8cdc02122229e8819de3b31f27c6a1ecd8d5f7
Alison Bartsch <abartsch@andrew.cmu.edu>
2025-07-06 13:04:01 -0400

    updated scripts

--- test_position_constraint.py
+++ test_position_constraint.py
@@ -78,5 +78,45 @@ maxy = pcl_maxs[1]
 # get the mean radius constraint
 r = np.mean([maxx-minx, maxy-miny]) - 0.03
 
+# create red point cloud of points along the circle with that radius
+theta = np.linspace(0, 2 * np.pi, 100)
+goal_x = r * np.cos(theta) + (maxx + minx) / 2
+goal_y = r * np.sin(theta) + (maxy + miny) / 2
+goal_z = np.zeros_like(goal_x) + (pcl_center[2] - 0.03) # slightly below the center of the point cloud
+goal_pcl = o3d.geometry.PointCloud()
+goal_pcl.points = o3d.utility.Vector3dVector(np.column_stack((goal_x, goal_y, goal_z)))
+goal_pcl.colors = o3d.utility.Vector3dVector(np.tile(np.array([1,0,0]), (len(goal_x),1)))
 
-# o3d.visualization.draw_geometries([pcl, goal_pcl])
+o3d.visualization.draw_geometries([pcl, goal_pcl])
+
+
+# alternatively, project all points in pointcloud into the x,y plane and fit a circle to those points
+# project points into x,y plane
+pcl_xy = pointcloud[:, :2]  # take only x and y components
+from sklearn.metrics import pairwise_distances
+# calculate pairwise distances
+distances = pairwise_distances(pcl_xy, metric='euclidean')
+# find the radius such that 95% of points are within that radius
+radius = np.percentile(distances, 95)
+print("Radius constraint: ", radius)
+# create green point cloud of points along the circle with that radius
+goal_x = radius * np.cos(theta) + (maxx + minx) / 2
+goal_y = radius * np.sin(theta) + (maxy + miny) / 2
+goal_z = np.zeros_like(goal_x) + (pcl_center[2] - 0.03) # slightly below the center of the point cloud
+goal_pcl = o3d.geometry.PointCloud()
+goal_pcl.points = o3d.utility.Vector3dVector(np.column_stack((goal_x, goal_y, goal_z)))
+goal_pcl.colors = o3d.utility.Vector3dVector(np.tile(np.array([0,1,0]), (len(goal_x),1)))
+o3d.visualization.draw_geometries([pcl, goal_pcl])
+
+# instead of using a circle constraint, we can use an ellipse constraint
+# create ellipse constraint
+ellipse_a = (maxx - minx) / 2
+ellipse_b = (maxy - miny) / 2
+# create red point cloud of points along the ellipse with that radius
+goal_x = ellipse_a * np.cos(theta) + (maxx + minx) / 2
+goal_y = ellipse_b * np.sin(theta) + (maxy + miny) / 2
+goal_z = np.zeros_like(goal_x) + (pcl_center[2] - 0.03) # slightly below the center of the point cloud
+goal_pcl = o3d.geometry.PointCloud()
+goal_pcl.points = o3d.utility.Vector3dVector(np.column_stack((goal_x, goal_y, goal_z)))
+goal_pcl.colors = o3d.utility.Vector3dVector(np.tile(np.array([1,0,0]), (len(goal_x),1)))
+o3d.visualization.draw_geometries([pcl, goal_pcl])
